1e274aae5c7195a2de12d3a755869e32a2603fb5
Hisu <letrunghoahieu@gmail.com>
2023-08-13 09:19:18 +0700

    Second commit

--- 2_QMainWindow/mainwindow.py
+++ 2_QMainWindow/mainwindow.py
@@ -1,7 +1,8 @@
 from typing import Optional
 import PySide6.QtCore
-from PySide6.QtWidgets import QApplication, QMainWindow
-
+from PySide6.QtCore import QSize
+from PySide6.QtGui import QAction, QIcon
+from PySide6.QtWidgets import QMainWindow, QToolBar, QPushButton, QStatusBar
 class MainWindow(QMainWindow):
     def __init__(self, app):
         super().__init__()
@@ -9,10 +10,50 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Custom MainWindow")
         
         menu_bar = self.menuBar()
-        file_menu = menu_bar.addMenu("&File")
+        file_menu = menu_bar.addMenu("File")
         quit_action = file_menu.addAction("Quit")
         quit_action.triggered.connect(self.quit_app)
 
+        edit_menu = menu_bar.addMenu("Edit")
+        edit_menu.addAction("Copy")
+        edit_menu.addAction("Cut")
+        edit_menu.addAction("Paste")
+        edit_menu.addAction("Undo")
+        edit_menu.addAction("Redo")
+
+        #Working with toolbars
+        toolbar = QToolBar("My main toolbar")
+        toolbar.setIconSize(QSize(16,16))
+        self.addToolBar(toolbar)
+
+        #Add the quit action to the toolbar
+        toolbar.addAction(quit_action)
+
+        action1 = QAction("Some action", self)
+        action1.setStatusTip("Status message for some action")
+        action1.triggered.connect(self.toolbar_button_click)
+        toolbar.addAction(action1)
+
+        action2 = QAction(QIcon("home.png"), "Some other action", self)
+        action2.setStatusTip("Status message for some other action")
+        action2.triggered.connect(self.toolbar_button_click)
+        toolbar.addAction(action2)
+
+        toolbar.addSeparator()
+        toolbar.addWidget(QPushButton("Click here"))
+
+        #Working with status bar
+        self.setStatusBar(QStatusBar(self))
+
+        button1 = QPushButton("BUTTON1")
+        button1.clicked.connect(self.button1_clicked)
+        self.setCentralWidget(button1)
+
+    def button1_clicked(self):
+        print("Clicked on the button")
 
     def quit_app(self):
         self.app.quit()
+
+    def toolbar_button_click(self):
+        self.statusBar().showMessage("Message from my app",3000)
